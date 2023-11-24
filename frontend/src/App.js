8e3552dad82ecb87f6fa2fe88b8ef93df44ad339
nico-alv <nicolas.alvarez02@alumnos.ucn.cl>
2023-11-24 03:35:01 -0300

    feat: tailwind initialization and welcome page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-6">
+          Bienvenido al Sistema de Administración de Usuarios de Dumbo
+        </h1>
+        <div className="space-x-4">
+          <button className="bg-blue-500 text-white px-4 py-2 rounded">
+            Registrarse
+          </button>
+          <button className="bg-green-500 text-white px-4 py-2 rounded">
+            Iniciar Sesión
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
